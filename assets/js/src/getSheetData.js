0efecd2f84141ff77800715fc2db0f47b4171cb7
wdzajicek <tig.welder4@gmail.com>
2023-03-23 16:16:16 -0500

    Add US & International outbreak RSS feeds.

--- assets/js/src/getSheetData.js
+++ assets/js/src/getSheetData.js
@@ -1,21 +1,37 @@
 import createHtml from "./createHtml";
 
+const SPREADSHEET_ID = '1_fc2lDA-UBFxINhj1QaOZ07ZlLlufzg4lrOCoVKXwEI';
 const API_PARAMS = { // This is configuration for API call with spreadsheets that are setup as readonly
   'apiKey': 'AIzaSyAUAfvITHm6rfXD5hYwsGA-AYjc2onrm1g',
   'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest']
 };
-
-const SHEET_PARAMS = {
-  spreadsheetId: '1_fc2lDA-UBFxINhj1QaOZ07ZlLlufzg4lrOCoVKXwEI',
+const SHEET_PARAMS_NEWS = {
+  spreadsheetId: SPREADSHEET_ID,
   range: 'RSS Import'
+};
+const SHEET_PARAMS_US = {
+  spreadsheetId: SPREADSHEET_ID,
+  range: 'US Outbreaks'
+};
+const SHEET_PARAMS_WORLD = {
+  spreadsheetId: SPREADSHEET_ID,
+  range: 'International Outbreaks'
 };
 
 function getSheetData() {
   gapi.load('client', () => {
     gapi.client.init(API_PARAMS).then(() => {
-      return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS);
+      return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS_US);
     }).then(response => {
-      createHtml(response);
+      createHtml(response, 'Latest US Outbreaks', false);
+
+      return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS_WORLD);
+    }).then(response => {
+      createHtml(response, 'Latest International Outbreaks', false);
+
+      return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS_NEWS);
+    }).then(response => {
+      createHtml(response, 'Latest CDC News Releases', true);
     }, err => {
       console.error("Error trying to fetch the alert from gapi:", err);
     })
--- docs/assets/js/src/createHtml.js
+++ docs/assets/js/src/createHtml.js
@@ -5,24 +5,45 @@ const embolden = str => str.replace(
   `<strong>$1</strong>`
 );
 
-function createHtml(response) {
+const createId = str => {
+  return (
+    str
+      .replace(/\s/g, '-')
+      .toLowerCase()
+  );
+}
+
+function createHtml(response, captionString, isNewsReleasesBool) {
   const data = response.result.values;
   const tableRows = [...data].map(item => {
-    const [title, url, description] = item;
+    if (isNewsReleasesBool) {
+      const [title, url, description] = item;
 
-    return (
-      `\n<tr>
+      return (
+        `\n<tr>
   <td><a href="${url}" target="_blank" rel="noopener noreferrer">${title}</a></td>
   <td>${embolden(description)}</td>
 </tr>`
-    );
+      );
+    } else {
+      const [title, url, date, description] = item;
+
+      return (
+        `\n<tr>
+  <td><a href="${url}" target="_blank" rel="noopener noreferrer">${title}</a></td>
+  <td>${date}</td>
+  <td>${embolden(description)}</td>
+</tr>`
+      );
+    }
   });
 
-  const html = `<table>
-  <caption>Latest CDC News Releases</caption>
+  const html = `<table class="margin--bottom--2">
+  <caption id="${createId(captionString)}">${captionString}</caption>
   <thead>
     <tr>
       <th>Title</th>
+      ${!isNewsReleasesBool ? '<th>Date</th>' : ''}
       <th>Description</th>
     </tr>
   </thead>
@@ -31,7 +52,8 @@ function createHtml(response) {
   </tbody>
 </table>`;
   
-  PARENT.innerHTML = html;
+  // PARENT.innerHTML = html;
+  PARENT.insertAdjacentHTML('beforeend', html);
 }
 
 export default createHtml;
